69671b292852fcde35055d8d2811cf0238fe68d6
Rob M <ramueller11@gmail.com>
2022-01-22 16:29:10 -0500

    Create package

---
+++ py-numwords/__init__.py
@@ -0,0 +1,13 @@
+"""
+numwords.py 
+
+Interconvert words with whole numbers.
+
+Example: 
+    int2words('555') = 'five hundred fifty five'
+    words2int('one') = 1
+    words2int('first') = 1 
+    words2int('a hundred') = 100
+    words2int('none') = 0
+    words2int('negative ten') = -10
+"""
